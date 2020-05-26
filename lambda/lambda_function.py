77110a84cf623acfa5a355b3021ed8728f7f9fa9
konkeramaris <konkeramaris@gmail.com>
2020-05-26 19:08:30 +0300

    simplified code

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -11,58 +11,56 @@ SRC_EMAIL   = os.environ['SRC_EMAIL']
 DST_EMAIL   = os.environ['SRC_EMAIL']
 BASE_URL    = os.environ['BASE_URL']
 
-def getEpisodesFromShowInfo(showid, key, episodes, dates):
-    show = get(BASE_URL+'/shows/'+showid)
-    if key in show.json()['_links']:
-        epidodeUrl = show.json()['_links'][key]['href']
-        response = get(epidodeUrl)
-        episodes.append('s'+str(response.json()['season'])+'e'+str(response.json()['number']))
-        dates.append(response.json()['airdate'])
-    else:
-        episodes.append('N/A')
-        dates.append('N/A')
-    return episodes, dates
+CURRENT_DATE = str(datetime.now().date())
+EMAIL_SUBJECT = 'Your Weekly Series Airtime Updater'
 
-def getEpisodesFromEpisodeList(data, index, episodes, dates):
-    if index == 0:
-        episodes.append('N/A')
-        dates.append('N/A')
+def sendSESEmail(data):
+    response = client.send_email(Source=SRC_EMAIL,Destination={'ToAddresses': [DST_EMAIL],},
+                                Message={
+                                    'Body': {
+                                        'Html': {
+                                            'Charset': 'UTF-8', 
+                                            'Data': data,
+                                            }
+                                        },
+                                    'Subject': { 
+                                        'Charset': 'UTF-8', 
+                                        'Data': EMAIL_SUBJECT
+                                        }
+                                    })
+    return response['ResponseMetadata']['HTTPStatusCode']
+
+def getDatesFromEpisodeList(data, index):
+    if index == 0 or data[index]['airdate']=='':
+        return 'N/A'
     else:
-        episodes.append('s'+str(data[index]['season'])+'e'+str(data[index]['number']))
-        dates.append(data[index]['airdate'])
-    return episodes,dates
+        return data[index]['airdate']
+
+def getEpisodesFromEpisodeList(data, index):
+    if index == 0:
+        return 'N/A'
+    else:
+        return 's'+str(data[index]['season'])+'e'+str(data[index]['number'])
 
 nextDates = []; nextEp = []; prevDates = []; prevEp = []
-for i in ids:
-    response = get(BASE_URL+'/shows/'+i+'/episodes')
-    if response.json()[-1]['airdate'] =='':
-        prevEp, prevDates = getEpisodesFromShowInfo(i, 'previousepisode', prevEp, prevDates)
-        nextEp, nextDates = getEpisodesFromShowInfo(i, 'nextepisode', nextEp, nextDates)
-    else:
-        if response.json()[-1]['airdate'] > str(datetime.now().date()):
-            # If episode is later than the current day
-            # then show the last previous episode and the upcoming episode
-            print (response.json())
-            pointer = -1 
-            while response.json()[pointer]['airdate'] > str(datetime.now().date()):
-                pointer -=1
-            print (pointer)
-            prevEp, prevDates = getEpisodesFromEpisodeList(response.json(), pointer, prevEp, prevDates)
-            nextEp, nextDates = getEpisodesFromEpisodeList(response.json(), pointer+1, nextEp, nextDates)
-        else:
-            prevEp, prevDates = getEpisodesFromEpisodeList(response.json(), -1, prevEp, prevDates)
-            nextEp, nextDates = getEpisodesFromEpisodeList(response.json(), 0, nextEp, nextDates)
+for id in ids:
+    response = get(BASE_URL+'/shows/'+id+'/episodes').json()
+    pointer = -1
+    # Find the latest airdate that is not empty and previous that the current day
+    while (response[pointer]['airdate'] > CURRENT_DATE) or (response[pointer]['airdate'] == ''): pointer -=1
+    # response[pointer] is the previous episode
+    # response[pointer+1] is the next episode (if it exists)
+    prevEp.append(getEpisodesFromEpisodeList(response, pointer))
+    nextEp.append(getEpisodesFromEpisodeList(response, pointer+1))
+    prevDates.append(getDatesFromEpisodeList(response, pointer))
+    nextDates.append(getDatesFromEpisodeList(response, pointer+1))
 
 # Sort, Combine and Transpose List
 nextDates, series, nextEp, prevDates, prevEp = zip(*sorted(zip(nextDates, series, nextEp, prevDates, prevEp)))
 transposedList = list(map(list, zip(*[series, prevEp, prevDates, nextEp, nextDates])))
 
-def lambda_handler(event, context):    
-    response = client.send_email(
-        Destination={'ToAddresses': [DST_EMAIL],},
-        Message={
-            'Body': {'Html': {'Charset': 'UTF-8', 'Data': tabulate(transposedList, headers=['Name', 'Prev. Ep.','Airtime','Next Ep.','Airtime'], colalign=("left","center","left","center","left"), tablefmt="html"),}},
-            'Subject': { 'Charset': 'UTF-8', 'Data': 'Your Weekly Series Airtime Updater',},
-        },
-        Source=SRC_EMAIL)
-    return response['ResponseMetadata']['HTTPStatusCode']
+def lambda_handler(event, context):
+    data = tabulate(transposedList, headers=['Name', 'Prev. Ep.','Airtime','Next Ep.','Airtime'], 
+                                    colalign=("left","center","left","center","center"), tablefmt="html")
+    response = sendSESEmail(data)
+    return response
